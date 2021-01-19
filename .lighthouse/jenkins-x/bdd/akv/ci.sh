4335c15e5a05d3ae7378c5d5a303f408eade3337
Chris Mellard <chrism@datacom.co.nz>
2021-01-20 00:37:47 +1300

    x

--- .lighthouse/jenkins-x/bdd/akv/ci.sh
+++ .lighthouse/jenkins-x/bdd/akv/ci.sh
@@ -11,10 +11,9 @@ export GITOPS_TEMPLATE_PROJECT="jx3-gitops-repositories/jx3-azure-akv"
 
 # enable the terraform azure key vault config
 export TF_VAR_key_vault_enabled=true
+export GH_OWNER="mellard-org"
+export GIT_USERNAME="chris-mellard"
 
-#`dirname "$0"`/../terraform-ci.sh
-
-echo "hello world"
-
+`dirname "$0"`/../terraform-ci.sh
 
 export CLUSTER_NAME="${BRANCH_NAME,,}-$BUILD_NUMBER-$BDD_NAME"
